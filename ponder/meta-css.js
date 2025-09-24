ac6c51882800d4b5f9b56ebf7e82cad7613a0750
Trevor Stephenson <trevors19themail@gmail.com>
2025-09-24 12:08:32 -0600

    big update

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -31,8 +31,17 @@ if (true) {
 
 
 let selectElem = document.getElementById('webdevlist');
+document.querySelector('#html').style.color = 'black';
+document.querySelector('#css').style.color = 'black';
+document.querySelector('#js').style.color = 'black';
 selectElem.addEventListener('change', function () {
     let codeValue = selectElem.value;
     console.log(codeValue);
     document.getElementById(codeValue).style.color = 'red';
 })
+
+const image = document.querySelector('img');
+image.setAttribute('src', 'images/html-js-css.jpg');
+image.setAttribute('alt', 'new logo with black backround')
+
+document.body.style.backgroundColor = 'black';
