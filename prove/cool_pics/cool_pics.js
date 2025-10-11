e941c6247b6e2dbb5b42ab24e26af6431faf0d1c
Trevor Stephenson <trevors19themail@gmail.com>
2025-10-11 14:04:18 -0600

    Fixed the menu

--- prove/cool_pics/cool_pics.js
+++ prove/cool_pics/cool_pics.js
@@ -29,4 +29,15 @@ modal.addEventListener('click', (event) => {
         modal.close();
     }
 });
+
+let btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+
+btn.addEventListener('click', togglemenu);
+
+function togglemenu() {
+    menu.classList.toggle('hide');
+    btn.classList.toggle('change');
+}
           
